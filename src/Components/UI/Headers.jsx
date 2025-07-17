31a725c8902ea01f3eb2858465602eac3bbed2f6
Afjal Hayat <afjalhayat2210@gmail.com>
2025-07-17 12:51:11 +0530

    Changes int the heading and the banner image

--- src/Components/UI/Headers.jsx
+++ src/Components/UI/Headers.jsx
@@ -1,28 +1,36 @@
-  import React from 'react'
-  import{NavLink} from "react-router-dom"
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
 
-  function Headers() {
-    return (
-      <header>
-        <div className='container'>
-          <div className="grid navbar-grid">
-            <div className="logo">
+function Headers() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <div className='container'>
+        <div className="grid navbar-grid">
+          <div className="logo">
             <NavLink to="/">
-            <h1>WorldAtlas</h1>
+              <h1>WorldAtlas</h1>
             </NavLink>
-            </div>
-            <nav>
-              <ul>
-                <li><NavLink to="/">HOME</NavLink></li>
-                <li><NavLink to="/About">ABOUT</NavLink></li>
-                <li><NavLink to="/Country">COUNTRY</NavLink></li>
-                <li><NavLink to="/Contact">CONTACT</NavLink></li>
-              </ul>
-            </nav>
           </div>
-        </div>
-      </header>
-    )
-  }
 
-  export default Headers
+          <nav>
+            <ul className={menuOpen ? 'mobile-nav' : 'nav-links'}>
+              <li><NavLink to="/" onClick={() => setMenuOpen(false)}>HOME</NavLink></li>
+              <li><NavLink to="/About" onClick={() => setMenuOpen(false)}>ABOUT</NavLink></li>
+              <li><NavLink to="/Country" onClick={() => setMenuOpen(false)}>COUNTRY</NavLink></li>
+              <li><NavLink to="/Contact" onClick={() => setMenuOpen(false)}>CONTACT</NavLink></li>
+            </ul>
+
+            <div className="ham-menu" onClick={() => setMenuOpen(!menuOpen)}>
+              {menuOpen ? "✖" : "☰"}
+            </div>
+          </nav>
+
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Headers;
